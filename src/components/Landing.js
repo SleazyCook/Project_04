bc56c96333523a38d601e6d76ef274c5fe6c7cc0
Andrew Cook <andrewjcook93@gmail.com>
2022-10-17 20:38:41 -0500

    fixed home page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
     return (
         <div>
-            <image alt="Dog playing sports" src="http://cdn.shopify.com/s/files/1/0275/2069/6387/articles/Choosing_a_Dog_Sport.jpg?v=1635728273"></image>
+            <img id="home-img" alt="Dog playing sports" width="600" height="400"src="http://cdn.shopify.com/s/files/1/0275/2069/6387/articles/Choosing_a_Dog_Sport.jpg?v=1635728273"></img>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ const router = createBrowserRouter([
             {
                 path: "/puppies",
                 element: <AllPuppies />
+            },
+            {
+                path: "/home",
+                element: <Landing />
             }
         ]
     }
